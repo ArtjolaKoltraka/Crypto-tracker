dd0b24ab2912abe10c3e72d7def4faa57c6bf1fd
ArtjolaK <koltrakaartjola@gmail.com>
2025-06-23 22:46:12 +0200

    optimize pagination while filtering

--- src/hooks/useMarketData.ts
+++ src/hooks/useMarketData.ts
@@ -1,10 +1,11 @@
-import { useState, useEffect, useMemo } from "react";
+import { useState, useEffect } from "react";
 import { useSearchStore } from "../store/useSearchStore";
 import { fetchMarketData, CoinLoreProps } from "../api/coinLore";
 
 export default function useMarketData(perPage = 20, page = 1) {
   const search = useSearchStore((s) => s.search.trim().toLowerCase());
   const [allCoins, setAllCoins] = useState<CoinLoreProps[]>([]);
+  const [filteredCoins, setFilteredCoins] = useState<CoinLoreProps[]>([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<Error | null>(null);
 
@@ -19,26 +20,34 @@ export default function useMarketData(perPage = 20, page = 1) {
       .finally(() => setLoading(false));
   }, []);
 
-  const filtered = useMemo(() => {
-    if (loading || !allCoins.length) return [];
-    if (!search) return allCoins;
-    return allCoins.filter(
-      (c) =>
-        c.name?.toLowerCase().includes(search) ||
-        c.symbol?.toLowerCase().includes(search)
-    );
-  }, [allCoins, search, loading]);
+  useEffect(() => {
+    if (!search) {
+      setFilteredCoins(allCoins);
+    } else {
+      setFilteredCoins(
+        allCoins.filter(
+          (c) =>
+            c.name?.toLowerCase().includes(search) ||
+            c.symbol?.toLowerCase().includes(search)
+        )
+      );
+    }
+  }, [search, allCoins]);
 
-  const paginated = useMemo(() => {
-    const start = (page - 1) * perPage;
-    return filtered.slice(start, start + perPage);
-  }, [filtered, page, perPage]);
+  const start = (page - 1) * perPage;
+  const paginated = filteredCoins.slice(start, start + perPage);
+  const total = filteredCoins.length;
 
-  const total = filtered.length;
+  const pagination = {
+    total: filteredCoins.length,
+    current_page: page,
+    per_page: perPage,
+    last_page: Math.ceil(total / perPage),
+  };
 
   return {
     data: paginated,
-    total,
+    pagination,
     loading,
     error,
   };
--- src/pages/Market.tsx
+++ src/pages/Market.tsx
@@ -1,32 +1,29 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import useMarketData from "../hooks/useMarketData";
 import CustomPagination from "../helpers/CustomPagination";
 import CoinTable from "../components/CoinTable";
+import { useSearchStore } from "../store/useSearchStore";
 
 const Market: React.FC = () => {
   const perPage = 20;
   const [activePage, setActivePage] = useState(1);
+  const search = useSearchStore((s) => s.search.trim().toLowerCase());
   const {
     data: coins,
-    total,
+    pagination,
     loading,
     error,
   } = useMarketData(perPage, activePage);
-  const lastPage = Math.ceil(total / perPage);
+
+  useEffect(() => {
+    setActivePage(1);
+  }, [search]);
 
   return (
     <>
       <div className="flex flex-col items-center px-12 mb-5 ">
         <CoinTable coins={coins} loading={loading} error={error} />
-        <CustomPagination
-          data={{
-            total,
-            current_page: activePage,
-            per_page: perPage,
-            last_page: lastPage,
-          }}
-          setActivePage={setActivePage}
-        />
+        <CustomPagination data={pagination} setActivePage={setActivePage} />
       </div>
     </>
   );
